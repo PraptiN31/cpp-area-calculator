56af393cbd623e4e4ead0319c49d4d8da6a8e70f
PraptiN31 <myselfprapti29@gmail.com>
2025-05-17 20:03:51 +0530

    Create circle-area.cpp

---
+++ circle-area.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main (){  
+  cout<<"Aim: To find the area of a circle"<<endl<<endl<<"Enter the radius = "; 
+  float n; 
+  cin>>n;
+  float sq = 3.14*n*n;
+  cout<<"The area of the square is = "<<sq;
+}
